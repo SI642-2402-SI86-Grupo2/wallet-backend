50c20c3993f7d20cff12a865af4fbfd6bafd5fcc
delilux <142470442+delilux@users.noreply.github.com>
2024-08-25 11:24:03 -0500

    Add files via upload

---
+++ src/main/java/pe/edu/pe/walletbackend1/Walletbackend1Application.java
@@ -0,0 +1,13 @@
+package pe.edu.pe.walletbackend1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Walletbackend1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Walletbackend1Application.class, args);
+    }
+
+}
