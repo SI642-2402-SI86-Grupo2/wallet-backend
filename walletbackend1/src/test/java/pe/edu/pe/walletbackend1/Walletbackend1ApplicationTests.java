e3d57e21a8338d4f36855cb77b51a545455d0494
delilux <142470442+delilux@users.noreply.github.com>
2024-08-25 11:21:08 -0500

    Add files via upload

---
+++ walletbackend1/src/test/java/pe/edu/pe/walletbackend1/Walletbackend1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.pe.walletbackend1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Walletbackend1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
