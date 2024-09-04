26d552708a0a9766a2318128215156ae096d5b10
delilux <142470442+delilux@users.noreply.github.com>
2024-09-03 22:36:44 -0500

    Revert "Create IPortfoliosRepository.java"
    This reverts commit dc2674a00bea06716e6b9cfa44fa004f1759bfc5.

--- src/main/java/pe/edu/pe/walletbackend1/repositories/IPortfoliosRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.pe.walletbackend1.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.pe.walletbackend1.entities.Portfolios;
-
-@Repository
-public interface IPortfoliosRepository extends JpaRepository<Portfolios, Integer> {
-}
