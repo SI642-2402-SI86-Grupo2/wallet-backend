bf5203160d9a5c14ca24003cbf7017ed6aebc369
delilux <142470442+delilux@users.noreply.github.com>
2024-09-03 19:48:41 -0500

    Create UsersDTO.java

---
+++ src/main/java/pe/edu/pe/walletbackend1/dtos/UsersDTO.java
@@ -0,0 +1,66 @@
+package pe.edu.pe.walletbackend1.dtos;
+
+public class UsersDTO {
+
+    private int userid;
+
+    private String firstname;
+
+    private String lastname;
+
+    private String email;
+
+    private String password;
+
+    private byte[] image;
+
+    //Setters and getters
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+}
