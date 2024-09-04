b84aeab7b551f81b2472170940e714313a50c863
delilux <142470442+delilux@users.noreply.github.com>
2024-09-03 22:45:09 -0500

    Update Portfolios Services class

---
+++ src/main/java/pe/edu/pe/walletbackend1/serviceinterface/IPortfoliosService.java
@@ -0,0 +1,15 @@
+package pe.edu.pe.walletbackend1.serviceinterface;
+
+import pe.edu.pe.walletbackend1.entities.Portfolios;
+import pe.edu.pe.walletbackend1.entities.Users;
+
+public interface IPortfoliosService {
+
+    public Portfolios listId(int id);
+
+    public void insert (Portfolios p);
+
+    public void update (Portfolios p);
+
+    public void delete (int id_portfolio);
+}
