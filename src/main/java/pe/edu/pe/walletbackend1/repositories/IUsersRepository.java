e6e2964f20cf2cff3e06b99be509d7151bf2998e
delilux <142470442+delilux@users.noreply.github.com>
2024-09-03 19:50:37 -0500

    Create IUsersRepository.java

---
+++ src/main/java/pe/edu/pe/walletbackend1/repositories/IUsersRepository.java
@@ -0,0 +1,10 @@
+package pe.edu.pe.walletbackend1.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pe.edu.pe.walletbackend1.entities.Users;
+
+@Repository
+public interface IUsersRepository extends JpaRepository<Users, Integer> {
+    
+}
